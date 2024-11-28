e14422ff0850bf3af67a95130e94696a6e59c51b
fahdjamy <fahdjamy2@gmail.com>
2024-11-27 23:04:53 -0600

    Add a health check endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("xrf197ilz35aq1 running...");
+use actix_web::{web, App, HttpResponse, HttpServer};
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let port = 8080;
+    let host = "127.0.0.1";
+    let address = format!("{}:{}", host, port);
+
+    HttpServer::new(|| {
+        App::new().route("/health", web::get().to(get_app_health))
+    })
+        .bind(address)?
+        .run()
+        .await
+}
+
+async fn get_app_health() -> HttpResponse {
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .body("healthy")
 }
