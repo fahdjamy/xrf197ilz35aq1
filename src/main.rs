6ff1f0390bd85f1da80811763df8e30b7407dc16
fahdjamy <fahdjamy2@gmail.com>
2024-11-27 23:53:41 -0600

    Add tracing. to be used to telemetry

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use actix_web::{web, App, HttpResponse, HttpServer};
+use tracing::instrument;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    tracing_setup();
+
     let port = 8080;
     let host = "127.0.0.1";
     let address = format!("{}:{}", host, port);
@@ -14,8 +17,16 @@ async fn main() -> std::io::Result<()> {
         .await
 }
 
+#[instrument]
 async fn get_app_health() -> HttpResponse {
+    tracing::info!("GET /health");
     HttpResponse::Ok()
         .content_type("application/json")
         .body("healthy")
+}
+
+fn tracing_setup() {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .init();
 }
