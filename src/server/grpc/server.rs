fae6a9bc9fb574720b00642c1dcb6cc7ed764216
fahdjamy <fahdjamy2@gmail.com>
2024-12-05 22:37:02 -0600

    Implement AssetServiceManager trait from the grpc asset server generated code

--- src/server/grpc/server.rs
+++ src/server/grpc/server.rs
@@ -1,17 +1,35 @@
+use crate::core::{create_new_asset, Asset, DomainError};
 use crate::server::grpc::server::asset::asset_service_server::AssetService;
 use crate::server::grpc::server::asset::{CreateRequest, CreateResponse};
+use sqlx::PgPool;
 use tonic::{Request, Response, Status};
 
 pub mod asset {
-    tonic::include_proto!("asset");
+    tonic::include_proto!("asset_rpc");
 }
 
 #[derive(Debug)]
-pub struct AsserService {}
+pub struct AssetServiceManager {
+    pg_pool: PgPool,
+}
 
 #[tonic::async_trait]
-impl AssetService for AsserService {
-    async fn create(&self, _: Request<CreateRequest>) -> Result<Response<CreateResponse>, Status> {
-        todo!()
+impl AssetService for AssetServiceManager {
+    async fn create(&self, request: Request<CreateRequest>) -> Result<Response<CreateResponse>, Status> {
+        let req = request.into_inner();
+        let asset = Asset::new(req.name, req.symbol, req.description, req.organization)
+            .map_err(|e| match e {
+                DomainError::DatabaseError(err) => Status::internal(err.to_string()),
+                DomainError::NotFoundError(err) => Status::not_found(err.to_string()),
+                DomainError::DuplicateError(err) => Status::already_exists(err.to_string()),
+                DomainError::InvalidArgument(err) => Status::invalid_argument(err.to_string()),
+                DomainError::ValidationError(err) => Status::invalid_argument(err.to_string()),
+            })?;
+        let asset_create_resp = create_new_asset(&asset, &self.pg_pool).await;
+        if let Err(err) = asset_create_resp {
+            return Err(Status::internal(err.to_string()));
+        }
+        let response = CreateResponse { asset_id: asset.id };
+        Ok(Response::new(response))
     }
 }
