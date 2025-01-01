a70f3f24a5a2c32064a9dbdf2784c902a6da71ff
fahdjamy <fahdjamy2@gmail.com>
2024-12-31 18:34:42 -0600

    Add method queries to delete and update an asset

--- src/core/queries/asset.rs
+++ src/core/queries/asset.rs
@@ -1,6 +1,6 @@
-use crate::core::{Asset, DatabaseError, OrderType};
+use crate::core::{Asset, DatabaseError, OrderType, UpdateAssetRequest};
 use anyhow::anyhow;
-use sqlx::PgPool;
+use sqlx::{PgPool, QueryBuilder};
 use tracing::error;
 
 #[tracing::instrument(level = "debug", skip(pg_pool, asset), name = "Create new asset")]
@@ -15,9 +15,10 @@ pub async fn create_new_asset(asset: &Asset, pg_pool: &PgPool) -> Result<bool, a
             description,
             organization,
             created_at,
-            updated_at
+            updated_at,
+            updated_by
         )
-        VALUES ($1, $2, $3, $4, $5, $6, $7)
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
         ",
         asset.id,
         asset.name,
@@ -25,7 +26,8 @@ pub async fn create_new_asset(asset: &Asset, pg_pool: &PgPool) -> Result<bool, a
         asset.description,
         asset.organization,
         asset.created_at,
-        asset.updated_at
+        asset.updated_at,
+        asset.updated_by,
     )
         .execute(pg_pool)
         .await
@@ -43,7 +45,7 @@ pub async fn find_asset_by_id(asset_id: &str, pg_pool: &PgPool) -> Result<Asset,
         Asset,
         r#"
         SELECT
-            id, name, symbol, description, organization, created_at, updated_at, tradable, listable
+            id, name, symbol, description, organization, created_at, updated_at, tradable, listable, updated_by
         FROM asset
         WHERE id = $1"#,
         asset_id
@@ -66,7 +68,7 @@ pub async fn get_all_assets(pg_pool: &PgPool, offset: i64, limit: i64, order_by:
                 Asset,
                 r#"
                 SELECT
-                    id, name, symbol, description, organization, created_at, updated_at, tradable, listable
+                    id, name, symbol, description, organization, created_at, updated_at, tradable, listable, updated_by
                 FROM asset
                 ORDER BY name ASC
                 LIMIT $1 OFFSET $2
@@ -82,7 +84,7 @@ pub async fn get_all_assets(pg_pool: &PgPool, offset: i64, limit: i64, order_by:
                 Asset,
                 r#"
                 SELECT
-                    id, name, symbol, description, organization, created_at, updated_at, tradable, listable
+                    id, name, symbol, description, organization, created_at, updated_at, tradable, listable, updated_by
                 FROM asset
                 ORDER BY name DESC
                 LIMIT $1 OFFSET $2
@@ -105,14 +107,14 @@ pub async fn find_assets_symbol_like(symbol: &str, limit: i16, offset: i64, orde
     // OR
     // 2. Trigram matching: Efficient for finding similar strings, even with typos or partial matches
     let search_term = format!("%{}%", sanitize_search_term(symbol).to_uppercase());
-    tracing::debug!("fetching assets from DB :: symbol={}", &search_term);
+    tracing::debug!("fetching assets from DB :: symbol = {}", &search_term);
     let result = match order_by {
         OrderType::Asc => {
             sqlx::query_as!(
                 Asset,
                 r#"
                 SELECT
-                    id, name, symbol, description, organization, created_at, updated_at, tradable, listable
+                    id, name, symbol, description, organization, created_at, updated_at, tradable, listable, updated_by
                 FROM asset
                 WHERE symbol LIKE $1
                 ORDER BY symbol ASC
@@ -130,7 +132,7 @@ pub async fn find_assets_symbol_like(symbol: &str, limit: i16, offset: i64, orde
                 Asset,
                 r#"
                 SELECT
-                    id, name, symbol, description, organization, created_at, updated_at, tradable, listable
+                    id, name, symbol, description, organization, created_at, updated_at, tradable, listable, updated_by
                 FROM asset
                 WHERE symbol LIKE $1
                 ORDER BY symbol DESC
@@ -157,7 +159,7 @@ pub async fn find_assets_name_like(name: &str, offset: i64, limit: usize, order_
                 Asset,
                 r#"
                 SELECT
-                    id, name, symbol, description, organization, created_at, updated_at, tradable, listable
+                    id, name, symbol, description, organization, created_at, updated_at, tradable, listable, updated_by
                 FROM asset
                 WHERE name LIKE $1
                 ORDER BY name ASC
@@ -173,7 +175,7 @@ pub async fn find_assets_name_like(name: &str, offset: i64, limit: usize, order_
                 Asset,
                 r#"
                 SELECT
-                    id, name, symbol, description, organization, created_at, updated_at, tradable, listable
+                    id, name, symbol, description, organization, created_at, updated_at, tradable, listable, updated_by
                 FROM asset
                 WHERE name LIKE $1
                 ORDER BY name DESC
@@ -187,6 +189,68 @@ pub async fn find_assets_name_like(name: &str, offset: i64, limit: usize, order_
     };
 
     Ok(result)
+}
+
+#[tracing::instrument(level = "debug", skip(pg_pool))]
+pub async fn delete_asset_by_id(asset_id: &str, pg_pool: &PgPool) -> Result<bool, DatabaseError> {
+    tracing::debug!("deleting asset :: id = {}", asset_id);
+    let result = sqlx::query!(
+        "
+        DELETE FROM asset
+        WHERE id = $1
+        ",
+        asset_id
+    )
+        .execute(pg_pool)
+        .await?;
+    Ok(result.rows_affected() == 1)
+}
+
+#[tracing::instrument(level = "debug", skip(pg_pool, asset))]
+pub async fn update_asset(asset_id: &str, asset: &UpdateAssetRequest, pg_pool: &PgPool) -> Result<bool, DatabaseError> {
+    let mut query_builder = QueryBuilder::new("UPDATE asset SET ");
+
+    let mut set_clauses = Vec::new();
+    if let Some(name) = &asset.name {
+        set_clauses.push(format!("name = '{}'", name.replace('\'', "''")));
+    }
+    if let Some(symbol) = &asset.symbol {
+        set_clauses.push(format!("symbol = '{}'", symbol.replace('\'', "''")));
+    }
+    if let Some(description) = &asset.description {
+        set_clauses.push(format!("description = '{}'", description.replace('\'', "''")));
+    }
+    if let Some(listable) = asset.listable {
+        set_clauses.push(format!("listable = {}", listable));
+    }
+    if let Some(tradable) = asset.tradable {
+        set_clauses.push(format!("tradable = {}", tradable));
+    }
+    set_clauses.push(format!("updated_at = '{}'", asset.updated_at));
+    if let Some(organization) = &asset.organization {
+        set_clauses.push(format!("organization = '{}'", organization.replace('\'', "''")));
+    }
+    set_clauses.push(format!("updated_by = '{}'", asset.updated_by.replace('\'', "''")));
+
+    query_builder.push(set_clauses.join(", "));
+    query_builder.push(" WHERE id = ");
+    query_builder.push_bind(asset_id); // Use bind for the ID
+
+    let query = query_builder.build();
+
+    match query.execute(pg_pool).await {
+        Ok(res) => {
+            if res.rows_affected() > 0 {
+                Ok(true)
+            } else {
+                Ok(false)
+            }
+        }
+        Err(e) => {
+            error!("Error executing SQL query: {:?}", e);
+            Err(DatabaseError::from(e))
+        },
+    }
 }
 
 fn sanitize_search_term(search_term: &str) -> String {
