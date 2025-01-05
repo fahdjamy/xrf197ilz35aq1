7c7b725e9d9271d6b9be0e969449c656ae483cb4
fahdjamy <fahdjamy2@gmail.com>
2025-01-04 19:02:06 -0600

    Add RequestId to hold request-id values

--- src/common.rs
+++ src/common.rs
@@ -5,3 +5,7 @@ pub const REQUEST_ID_KEY: &str = "request-id";
 pub fn generate_request_id() -> String {
     Uuid::new_v4().to_string()
 }
+
+// A simple extension to store the request ID in a span.
+#[derive(Clone, Debug)]
+pub struct RequestId(pub String);
