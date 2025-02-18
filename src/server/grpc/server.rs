893053be7ab863788b0ac5a5ca33bb288490eb3a
fahdjamy <fahdjamy2@gmail.com>
2025-02-17 23:21:57 -0600

    Add load_certs method to use when setting up TLS/SSL

--- src/server/grpc/server.rs
+++ src/server/grpc/server.rs
@@ -4,7 +4,12 @@ use crate::server::grpc::asset::asset_service_server::AssetServiceServer;
 use crate::server::grpc::asset::contract_service_server::ContractServiceServer;
 use crate::server::grpc::services::{AssetServiceManager, ContractServiceManager};
 use anyhow::Context;
+use rustls::pki_types::{CertificateDer, PrivateKeyDer};
+use rustls_pemfile::Item;
 use sqlx::PgPool;
+use std::fs::File;
+use std::io::BufReader;
+use std::path::Path;
 use std::str::FromStr;
 use std::sync::Arc;
 use std::time::Duration;
@@ -12,7 +17,7 @@ use tonic::metadata::{KeyAndValueRef, MetadataKey, MetadataValue};
 use tonic::transport::{Error, Server};
 use tonic::{Request, Status};
 use tower::ServiceBuilder;
-use tracing::{info, info_span};
+use tracing::{info, info_span, warn};
 
 pub struct GrpcServer {
     timeout: Duration,
@@ -88,4 +93,37 @@ impl GrpcServer {
 
         Ok(req)
     }
+}
+
+fn load_certs(cert_path: &Path, key_path: &Path)
+              -> anyhow::Result<(Vec<CertificateDer<'static>>, PrivateKeyDer<'static>)> {
+
+    // Load certificate(s).
+    let cert_file = File::open(cert_path).context("Failed to open certificate file")?;
+    let mut reader = BufReader::new(cert_file);
+    let mut certs = Vec::new();
+    while let Ok(Some(item)) = rustls_pemfile::read_one(&mut reader) {
+        if let rustls_pemfile::Item::X509Certificate(cert) = item {
+            certs.push(cert.into_owned()); // Convert to CertificateDer<'static>
+        } else {
+            // Log and ignore other items, or handle them appropriately
+            // use tracing::warn if you're using the tracing lib
+            warn!("Certificate is not a valid PEM encoded X509 certificate");
+        }
+    }
+
+    if certs.is_empty() {
+        anyhow::bail!("No certificates found in {}", cert_path.display());
+    }
+
+    // Load private key
+    let key_file = File::open(key_path).context("Failed to open private key file")?;
+    let mut key_reader = BufReader::new(key_file);
+    let key = match rustls_pemfile::read_one(&mut key_reader).context("Failed to read private key")? {
+        None => anyhow::bail!("No private key found in {}", key_path.display()),
+        Some(Item::Pkcs8Key(key)) => PrivateKeyDer::from(key.into()), // Convert to PrivateKeyDer
+        Some(_) => anyhow::bail!("Found an unexpected item in the key file. Expected a PKCS#8, Sec1, or RSA key."),
+    };
+
+    Ok((certs, key))
 }
