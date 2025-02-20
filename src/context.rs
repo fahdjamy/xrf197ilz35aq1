6c9d6b1e96f599cae0ec94e3d61906f97bed8018
fahdjamy <fahdjamy2@gmail.com>
2025-02-19 19:22:50 -0600

    Add application context to load application environment in a singleton way

--- src/context.rs
+++ src/context.rs
@@ -1,3 +1,8 @@
+use crate::common::XRF_ENV_KEY;
+use lazy_static::lazy_static;
+use std::sync::OnceLock;
+use uuid::Uuid;
+
 #[derive(Clone)]
 pub enum Environment {
     Dev,
@@ -22,6 +27,10 @@ impl Environment {
         };
         false
     }
+
+    fn is_not_local(&self) -> bool {
+        !self.is_local()
+    }
 }
 
 impl TryFrom<String> for Environment {
@@ -34,5 +43,29 @@ impl TryFrom<String> for Environment {
             "dev" | "local" => Ok(Environment::Dev),
             _ => Err(format!("Unknown environment: {}", env)),
         }
+    }
+}
+
+pub struct AppContext {
+    pub server_id: String,
+    pub environment: Environment,
+}
+
+lazy_static! {
+    pub static ref APP_CONTEXT_CONFIG: OnceLock<AppContext> = OnceLock::new();
+}
+
+impl AppContext {
+    pub fn load() -> &'static AppContext {
+        APP_CONTEXT_CONFIG.get_or_init(|| {
+            // Determine the environment.  This is where your loading logic goes.
+            // For this example, we'll use an environment variable.
+            let env: Environment = std::env::var(XRF_ENV_KEY)
+                .unwrap_or_else(|_| "dev".into())
+                .try_into()
+                .expect("XRF_ENV env variable is not accepted environment");
+
+            AppContext { environment: env.clone(), server_id: Uuid::new_v4().to_string() }
+        })
     }
 }
