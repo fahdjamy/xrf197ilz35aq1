b910e4034295d2c4c9654f1dcf2e3a7071220e80
fahdjamy <fahdjamy2@gmail.com>
2025-07-28 18:13:30 -0500

    bump dependency version

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/asset.proto")?;
-    tonic_build::compile_protos("proto/contract/v1/contract.proto")?;
+    tonic_prost_build::compile_protos("proto/asset.proto")?;
+    tonic_prost_build::compile_protos("proto/contract/v1/contract.proto")?;
     Ok(())
 }
