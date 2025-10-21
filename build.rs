659ef6b884ce64a8af19194693dac368d9fe7516
fahdjamy <fahdjamy2@gmail.com>
2025-10-21 17:35:45 -0500

    Move asset proto file in an asset/v1 dir

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_prost_build::compile_protos("proto/asset.proto")?;
+    tonic_prost_build::compile_protos("proto/asset/v1/asset.proto")?;
     tonic_prost_build::compile_protos("proto/contract/v1/contract.proto")?;
     Ok(())
 }
